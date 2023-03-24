fa5576b8fc439a54ae71f828b28e0281f636524e
w.forcignano <w.forcignano@reply.it>
2023-03-24 16:45:03 +0100

    First commit

---
+++ wallbox-management/src/main/java/com/tme/wallboxmanagement/WallboxManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tme.wallboxmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallboxManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallboxManagementApplication.class, args);
+	}
+
+}
